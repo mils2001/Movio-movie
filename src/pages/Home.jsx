229bd8c332c300774dfeab75535c23622351b14a
chris <Mileschris466@gmail.com>
2024-11-22 14:04:04 +0300

    additional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home (){
     return(
         <div>
-            
+           <p>Welcome to movio App..Home of Onstream Movie Stream  App</p> 
         </div>
     )
 }
